36b9c32bc6376dec0f9af0294374eb24746437df
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2025-10-28 23:17:40 +0530

    make the backend and connect the database

---
+++ Server/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express=require('express');
+const app=express();
+const PORT=process.env.PORT;
+const database=require('./config/database');
+
+app.use(express.json());
+database();
+
+
+app.get('/',(req,res)=>{
+    res.json({messge:"this is the home page"});
+});
+
+app.listen(PORT,(err)=>{
+    if(err){
+        console.error(err.message);
+    }else{
+        console.info("server is runing")
+    }
+});
